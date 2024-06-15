22b73a31dd780d73a79aa83c15715f734be737f9
unknown <meghanamaheshwari1@gmail.com>
2024-06-15 22:19:52 +0530

    changes

--- server.py
+++ server.py
@@ -28,12 +28,14 @@ def client_handler(client):
     while 1:
         username = client.recv(2048).decode('utf-8')
         if username != '':
-            active_clients.append(username, client)
+            active_clients.append((username, client))
+            prompt_message = "SERVER-" + f"{username} added to the chat"
+            send_messages_to_all(prompt_message)
             break
         else:
             print("client username is empty")
 
-            threading.Thread(target= listen_for_messages, args= (client, username, )).start()
+    threading.Thread(target= listen_for_messages, args= (client, username, )).start()
 
 #main
 def main():
@@ -52,7 +54,7 @@ def main():
         client, address = server.accept()
         print(f"Successfully conected to {address[0]} {address[1]}")
 
-    threading.Thread(target=client_handler, args=(client, )).start()
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 if __name__ == '__main__':
     main()
